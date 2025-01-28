8feab7a79c8c85cd36f723752ae09e5e527d9b7b
Ken Ballon <bkenn003@gmail.com>
2025-01-28 15:36:12 +0800

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatSlideToggleModule, MatInputModule, MatSelectModule, MatDatepickerModule, MatNativeDateModule, MatFormFieldModule, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
+
 export class AppComponent {
-  title = 'ang-material';
+  // title = 'ang-material';
+  fb = inject(FormBuilder);
+
+  form = this.fb.group({  
+    name: [''],
+    email: [''],
+    password: [''],
+    confirmPassword: [''],
+    address: [''],
+    city: [''],
+    state: [''],
+    zip: [''],
+    country: [''],
+    phone: [''],
+    dob: ['']
+  });
 }
